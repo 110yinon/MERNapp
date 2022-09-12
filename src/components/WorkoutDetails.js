b0243134ea87846a488c896226148a682b4ac86f
goo <goo>
2022-09-12 18:43:55 +0300

    using date-fns packge

--- src/components/WorkoutDetails.js
+++ src/components/WorkoutDetails.js
@@ -1,4 +1,5 @@
 import useWorkoutContext from "../hooks/useWorkoutsContext";
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
 
 const WorkoutDetails = ({ workout }) => {
@@ -25,7 +26,11 @@ const WorkoutDetails = ({ workout }) => {
             <h4>{workout.title}</h4>
             <p><strong>Load (kg): </strong>{workout.load}</p>
             <p><strong>Reps: </strong>{workout.reps}</p>
-            <p>{workout.createdAt}</p>
+            <p>
+                {
+                    formatDistanceToNow(new Date(workout.createdAt),{addSuffix: true})
+                }
+            </p>
             <span class="material-symbols-outlined" onClick={handleClick}>
                 delete
             </span>
